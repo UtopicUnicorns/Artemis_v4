ed39da262630301c3b98ff18da4552b092f191c1
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2022-03-05 21:16:23 +0100

    Rewrite Git fetch script
    Nice new sorting system for git script, URL's made sane and included commit URL.
    Author picture is always visible now, as well as a good button link for author URL

--- scripts/git.js
+++ scripts/git.js
@@ -29,8 +29,18 @@ fetch('https://api.github.com/users/UtopicUnicorns/events')
 				let repo = { name: i.repo.name, url: i.repo.url.replace('api.', '').replace('/repos', ''), head: i.payload.ref };
 
 				if (i.payload.commits) {
-					var title = { name: i.payload.commits[0].message.split('\n\n')[0], comment: i.payload.commits[0].message.split('\n\n')[1] };
-				} else var title = { name: '=!', comment: '=!' };
+					var title = {
+						name: i.payload.commits[0].message.split('\n\n')[0],
+						comment: i.payload.commits[0].message.split('\n\n')[1],
+						commit: i.payload.commits[0].url.replace('api.', '').replace('/repos', '').replace('commits', 'commit'),
+					};
+				} else {
+					var title = {
+						name: '=!',
+						comment: '=!',
+						//commit '=!'
+					};
+				}
 
 				let target_name = '';
 
@@ -38,21 +48,20 @@ fetch('https://api.github.com/users/UtopicUnicorns/events')
 				if (repo.name.includes('Artemis')) target_name += './images/assets/project_arrow_background.png';
 				if (repo.name.includes('mint')) target_name += './images/assets/Server_Invite.png';
 				if (repo.head.includes('site')) target_name = './images/assets/project_artemis_background.png';
+
 				let combine_info = `
-				<table style="width: 100%; height: 15%; overflow: hidden; background-color: rgba(255, 255, 255, 0.8); border-radius: 5px; box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);">
+				<table style="width: 100%; height: 30%; overflow: hidden; background-color: rgba(255, 255, 255, 0.8); border-radius: 5px; box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);">
 					<tbody>
 						<tr>
-							<td style="text-align: center; background-image: url('${target_name}'); background-position: center; background-size: cover; background-repeat: no-repeat">
-								<table style="width: 100%; height: 100%;">
+							<td style="height: 80px;text-align: center; background-image: url('${target_name}'); background-position: center; background-size: cover; background-repeat: no-repeat">
+								<table style="width: 100%; height: 50%;">
 									<tbody>
 										<tr>
 											<td style="width: 50%; height: 100%; text-align: left; padding-left: 5%;">
-												<a href="${author.link}" target="_blank" class="pulse">${author.name}</a><br />
 												${title.name}<br />
 												${time}
 											</td>
-											<td style="text-align: right; padding-right: 5%; width: 50%; height: 100%; background-image: url('${author.pic}');background-position: right; background-size: contain; background-repeat: no-repeat;">
-												
+											<td style="text-align: right; padding-right: 5%; width: 50%; height: 100%; background-image: url('${author.pic}');background-position: right; background-size: contain; background-repeat: no-repeat;">	
 											</td>
 										</tr>
 									</tbody>
@@ -60,13 +69,15 @@ fetch('https://api.github.com/users/UtopicUnicorns/events')
 							</td>
 						</tr>
 						<tr>
-							<td>
+							<td style="padding-left: 5%;">
 								${title.comment}
 							</td>
 						</tr>
 						<tr>
-							<td>
-									<a href="${repo.url}" target="_blank" class="pulse">${repo.name}</a> / ${repo.head}
+							<td style="text-align: center;">
+								<button class="menu_button pulse" onclick="window.open('${repo.url}/tree/${repo.head.split("/")[2]}','_blank')" target="_blank">${repo.name}/${repo.head.split("/")[2]}</button>
+								<button class="menu_button pulse" onclick="window.open('${title.commit}','_blank')" target="_blank">View Commit</button>
+								<button class="menu_button pulse" onclick="window.open('${author.link}','_blank')" target="_blank">${author.name}</button>
 							</td>
 						</tr>
 					</tbody>
@@ -89,9 +100,9 @@ fetch('https://api.github.com/users/UtopicUnicorns/events')
 		<table style="width: 90%; height: 90%; overflow: hidden;">
 			<tbody>
 				<tr>
-					<td style="width: 42%; height: 100%; overflow: hidden;">${array.slice(0,5).join(' ')}</td>
+					<td style="width: 42%; height: 100%; overflow: hidden;">${array.slice(0,3).join(' ')}</td>
 					<td style="width: 6%;"></td>
-					<td style="width: 42%; height: 100%; overflow: hidden;">${array2.slice(0,5).join(' ')}</td>
+					<td style="width: 42%; height: 100%; overflow: hidden;">${array2.slice(0,3).join(' ')}</td>
 				</tr>
 			</tbody>
 		</table>
